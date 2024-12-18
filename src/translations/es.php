b229970cfb780be56f41a46b7ca183ea30b29bdf
Alberto Vargas <vargasmorenoalberto@gmail.com>
2024-12-18 01:39:09 -0600

    feat: add spanish translation

---
+++ src/translations/es.php
@@ -0,0 +1,31 @@
+<?php
+$translations = [
+    'name' => 'Nombre',
+    'email' => 'Correo electrónico',
+    'comment' => 'Comentario',
+
+    'emailHint' => 'El correo electrónico es opcional. Si se agrega, se utiliza para enviar notificaciones cuando hay una respuesta. Tu correo electrónico nunca será publico. Puedes cancelar el envío de notificaciones y eliminar tu dirección de correo electrónico mediante un enlace en la notificación.',
+
+    'submit' => 'Comentar',
+    'reply' => 'Responder',
+    'writeComment' => 'Escribe un comentario...',
+
+    'noCommentsYet' => 'Aún no hay comentarios. ¡Se el primero!',
+    'successMessage' => 'Comentario publicado exitosamente',
+    'failMessage' => "Tu comentario no se publicó. Razón: ",
+
+    'dateString' => 'hace {}',
+    'years' => 'años',
+    'months' => 'meses',
+    'days' => 'días',
+    'hours' => 'horas',
+    'minutes' => 'minutos',
+    'seconds' => 'segundos',
+
+    # reply notification e-mail
+    'subject' => '{} respondió a tu comentario',
+    'introduction' => 'Hay una nueva respuesta a tu comentario.',
+    'author' => 'Autor',
+    'message' => 'Mensaje',
+    'unsubscribeDescription' => 'Para dejar de recibir notificaciones como esta, da clic al siguiente enlace:',
+];
